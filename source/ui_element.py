4ad1bdc6f551366e0ea37196cb5e582655857ea7
JV <jaiveer.chadda@gmail.com>
2026-04-02 22:20:22 +0100

    Cleaned up the UI Elems class

--- source/ui_element.py
+++ source/ui_element.py
@@ -4,38 +4,35 @@ from typing import Final, Optional
 
 class UIElement:
 
-    _TYPE_RE: Final[str] = '([^"]+?)'
-    _IDX_RE:  Final[str] = '(\\d+)'
-    _NAME_RE: Final[str] = '"(.+?)"'
+    _TYPE_REGEX: Final[str] = '([^"]+?)'
+    _IDX_REGEX:  Final[str] = '(\\d+)'
+    _NAME_REGEX: Final[str] = '"(.+?)"'
 
-    _MATCH_ALL_RE: Final[str] = f"{_TYPE_RE} (?:{_IDX_RE}|{_NAME_RE})$"
+    _MATCH_ALL_REGEX: Final[str] = f"{_TYPE_REGEX} (?:{_IDX_REGEX}|{_NAME_REGEX})$"
 
 
     def __init__(self: UIElement, string: str) -> None:
-        matches: Final[list[str]] = findall(self._MATCH_ALL_RE, string)[0]
+        _matches: Final[list[str]] = findall(self._MATCH_ALL_REGEX, string)[0]
 
-        self.type:          str  =     matches[0]
-        self.idx:  Optional[int] = int(matches[1]) if matches[1] else None
-        self.name: Optional[str] =     matches[2]  if matches[2] else None
-        
-        self.identifier:  str  = self.name if self.name else self.idx
-        self._id_is_name: bool = bool(self.name)
+        self.type:          str  =     _matches[0]
+        self.idx:  Optional[int] = int(_matches[1]) if _matches[1] else None
+        self.name: Optional[str] =     _matches[2]  if _matches[2] else None
 
-    def _id_formatted(self) -> str:
-        if self._id_is_name: return f'"{self.identifier}"'
-        return self.identifier
+        self.iden: str | int = self.name if self.name else self.idx
 
-    def _name_formatted(self) -> str:
-        return f'"{self.name}"' if self.name else 'None'
 
-    def __str__(self) -> str:
-        return f"{self.type} {self._id_formatted()}"
+    def _iden_formatted(self) -> str: return f'"{self.iden}"' if self.name else self.iden
+    def _name_formatted(self) -> str: return f'"{self.name}"' if self.name else None
+
+
+    def __str__(self) -> str: return f"{self.type} {self._iden_formatted()}"
 
     def __repr__(self) -> str:
-        params: list[str] = [
-            f"type: {self.type}",
+        return "".join((
+            "UIElement(",
+            f"type: {self.type}, ",
+
+            f"name: {self._name_formatted()}" if self.name else
             f"idx: {self.idx}",
-            f"name: {self._name_formatted()}",
-            f"identifier: {self._id_formatted()}"
-        ]
-        return f"UIElement({", ".join(params)})"
+            ")"
+        ))

b31d7ee043227864b5c456dfd569a977b0ded908
JV <jaiveer.chadda@gmail.com>
2026-04-02 23:35:29 +0100

    Did some more formatting and sectioning

--- source/formatter.py
+++ source/formatter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 from sys import argv
 from os.path import isfile, isdir, exists
 
@@ -8,11 +9,12 @@ from typing import Final
 from split import split_unquoted
 from ui_element import UIElement
 
-# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
+
+# ——— Constants ————————————————————————————————————————————————————————————————————————————————————————————————————— #
 
 ln_obj = list[dict[str, int | list[UIElement]]]
 
-# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
+# —————————————————————————————————————————————————— #
 
 ARGC: Final[int] = len(argv)
 
@@ -23,27 +25,42 @@ CLOSE_PAREN:  Final[str] = '}'
 COMMA_DELIM:  Final[str] = ", "
 OF_DELIM:     Final[str] = " of "
 
-# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
+INPUT_PROMPT: Final[str] = "Enter input filepath >>> "
 
-def read_file(_filepath: str) -> str:
+
+# ——— read_file ————————————————————————————————————————————————————————————————————————————————————————————————————— #
+
+def read_file(file: str) -> str:
+
+    FILE_NOT_FOUND_ERR_MSG:  Final[str] = f"Could not find file '{file}'."
+    IS_A_DIRECTORY_ERR_MSG:  Final[str] = f"{file} is a directory."
+    NOT_TEXT_FILE_ERR_MSG:   Final[str] = f"{file} is not a text file."
+    NOT_SINGLE_LINE_ERR_MSG: Final[str] = "File must be a single line."
+
+    # ——————————————————————————————————————————————————————— #
 
     # Check the path's validity
-    if not exists(_filepath): raise FileNotFoundError(f"Could not find file '{_filepath}'")
-    if 		isdir(_filepath): raise IsADirectoryError(f"{_filepath} is a directory")
-    if not isfile(_filepath): raise        ValueError(f"{_filepath} is not a text file")
+    if not exists(file): raise FileNotFoundError(FILE_NOT_FOUND_ERR_MSG)
+    if 		isdir(file): raise IsADirectoryError(IS_A_DIRECTORY_ERR_MSG)
+    if not isfile(file): raise        ValueError(NOT_TEXT_FILE_ERR_MSG)
 
     # Read the file
-    with open(_filepath, 'r') as f:
-        _file_contents: str = f.read().strip()
-
+    with open(file, 'r') as f:
+        _contents: str = f.read().strip()
+    
+    # ——————————————————————————————————————————————————————— #
+    
     # Check that the stripped file is just one line long
-    if _file_contents.count(NEWLINE) > 0: raise ValueError("File must be a single line")
+    if _contents.count(NEWLINE) > 0:
+        raise ValueError(NOT_SINGLE_LINE_ERR_MSG)
 
-    return _file_contents
+    return _contents
 
-# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
+
+# ——— clean_file ———————————————————————————————————————————————————————————————————————————————————————————————————— #
 
 def clean_file(_file_contents: str) -> str:
+    new_file_contents: str
 
     # If the file starts or ends with curly braces, remove them
     if _file_contents[0]  == OPEN_PAREN:  new_file_contents = _file_contents[1:]
@@ -51,12 +68,14 @@ def clean_file(_file_contents: str) -> str:
     
     return new_file_contents
 
-# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
 
+# ——— parse_file ———————————————————————————————————————————————————————————————————————————————————————————————————— #
 
 def parse_file(_file_contents: str) -> ln_obj:
     # Split the file at every unquoted instance of ", "
     _file_lines: Final[list[str]] = split_unquoted(COMMA_DELIM, _file_contents)
+
+    # ——————————————————————————————————————————————————————— #
 
     # Create a dictionary for each line,
     #  and split each line at " of "
@@ -73,15 +92,18 @@ def parse_file(_file_contents: str) -> ln_obj:
         
     return _lines_obj
 
-# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————— #
 
+# ——— main —————————————————————————————————————————————————————————————————————————————————————————————————————————— #
 
 def main() -> None:
-    filepath: Final[str] = argv[1] if ARGC >= 2 else input("Enter input filepath >>> ")
+
+    filepath: Final[str] = argv[1] if ARGC >= 2 else input(INPUT_PROMPT)
 
     raw_file_cont: Final[str]    =  read_file(filepath)
     file_contents: Final[str]    = clean_file(raw_file_cont)
     lines_object:  Final[ln_obj] = parse_file(file_contents)
+    
+    # ——————————————————————————————————————————————————————— #
 
     for line in lines_object:
         print(line["indent"], *map(str, line["content"]), sep='\t')
